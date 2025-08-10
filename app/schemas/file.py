78096b792f71ef87aca49536aa5dd8c1ea3feb6e
Anastasiia Nevodchikova <nd@kode.ru>
2025-08-10 16:51:20 +0300

    fix model_config for v2 pydantic

--- app/schemas/file.py
+++ app/schemas/file.py
@@ -1,12 +1,11 @@
 # schemas.py
-from pydantic import BaseModel, Field
 from typing import Optional
+from pydantic import BaseModel, Field, ConfigDict
 
 class FileBrief(BaseModel):
+    model_config = ConfigDict(from_attributes=True)  # замена orm_mode=True
+
     filename: str
     public_url: Optional[str] = None
-    project: Optional[str] = Field(None, alias="project_name")
-
-    class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
+    # читаем из атрибута/свойства ORM "project_name", а наружу возвращаем ключ "project"
+    project: Optional[str] = Field(None, validation_alias="project_name")
